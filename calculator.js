804292ff62063edf4de5bdef6304fe3b013c00b5
Husna AliJan <161767107+HusnaAliJan@users.noreply.github.com>
2024-03-23 00:49:32 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "FirstNumber" },
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+    {
+        message: "Selectr the Operator", type: "list", name: "operator", choices: ["addition", "substraction", "multiplication", "division"]
+    }
+]);
+if (answer.operator === "addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "substraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Ahad ki jaan Wrong Operator");
+}
